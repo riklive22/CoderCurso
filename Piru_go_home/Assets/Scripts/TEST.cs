c973e10034434f347fe2962029cabd678c2f71ba
riklive22 <ricardovico84@gmail.com>
2022-09-13 22:05:08 -0300

    v 0.0.1
    (Versión inicial del proyecto)
    Escena de unity vacia

---
+++ Piru_go_home/Assets/Scripts/TEST.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+
+public class TEST : MonoBehaviour
+{
+
+    public Vector3 movimiento;
+
+    public Vector3 rotation;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.Translate(movimiento);
+       
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        transform.Rotate(rotation); 
+    }
+
+
+}
---
+++ Piru_go_home/Assets/Scripts/Test2.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+public class Test2 : MonoBehaviour
+
+
+{
+
+   public Vector3 direccion; //variable Direccion
+
+   public float speed=1; //Variable Velocidad
+
+   int vida = 1; //metodo usado para disminuir  Variables de vida
+
+   float energia = 0.0f;  //metodo usado para subir Variables de vida
+   
+
+    // Start is called before the first frame update
+    void Start()
+    {
+     
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      transform.Translate(direccion);
+    }
+
+    
+    void subirvida()
+
+    {
+      energia += Time.deltaTime;
+      Debug.Log(energia);
+    }
+
+      void mover() //controla movimiento y velocidad
+    {
+        energia += Time.deltaTime;
+        transform.position += new Vector3(0.1f,0,0) * speed * Time.deltaTime;
+        
+    }
+
+    
+    void Quitarvida()
+
+    {
+      vida-=1;
+      Debug.Log(vida);
+    }
+
+}
